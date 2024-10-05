872b34969b2382d40b6c29d4bbb57bb6aca2248d
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-05 20:32:24 +0500

    feat: add list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const list = async () => {
-    // Write your code here 
+  const filesPath = join(__dirname, "files");
+
+  if (!fs.existsSync(filesPath)) {
+    throw new Error("FS operation failed");
+  } else {
+    fs.readdir(filesPath, (err, files) => {
+      if (err) {
+        throw new Error(err);
+      }
+
+      console.log(files);
+    });
+  }
 };
 
 await list();
