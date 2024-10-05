df67fa198fed51e9bc4e26a2238a8d0763e3d370
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-05 20:24:55 +0500

    feat: add delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const remove = async () => {
-    // Write your code here 
+  const removeFilePath = join(__dirname, "files", "fileToRemove.txt");
+
+  if (!fs.existsSync(removeFilePath)) {
+    throw new Error("FS operation failed");
+  } else {
+    fs.rm(removeFilePath, (err) => {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  }
 };
 
 await remove();
