4fa6a8bf4e3961b4fa50ae64177bb8e31122b085
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-05 10:53:12 +0500

    feat: add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+import { join } from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+  const content = "I am fresh and young";
+  const filePath = join(__dirname, "files", "fresh.txt");
+
+  if (fs.existsSync(filePath)) {
+    throw new Error("FS operation failed");
+  } else {
+    fs.writeFile(filePath, content, (err) => {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  }
 };
 
 await create();
