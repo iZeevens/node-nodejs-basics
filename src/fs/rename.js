55a8c69eb895f19e8f8350964df756d9f32ef6ee
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-05 20:20:35 +0500

    feat: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
+  const wrongFilePath = join(__dirname, "files", "wrongFilename.txt");
+  const propertyFilePath = join(__dirname, "files", "properFilename.txt");
+
+  if (!fs.existsSync(wrongFilePath) || fs.existsSync(propertyFilePath)) {
+    throw new Error("FS operation failed");
+  } else {
+    fs.rename(wrongFilePath, propertyFilePath, (err) => {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  }
 };
 
 await rename();
