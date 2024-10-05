f20acf31cf6d2a899ae7d7f4d80c4feb62a1a15c
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-05 20:36:40 +0500

    feat: add read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const readFilePath = join(__dirname, "files", "fileToRead.txt");
+
+  if (!fs.existsSync(readFilePath)) {
+    throw new Error("FS operation failed");
+  } else {
+    fs.readFile(readFilePath, { encoding: "utf-8" }, (err, data) => {
+      if (err) {
+        throw new Error(err);
+      }
+
+      console.log(data);
+    });
+  }
 };
 
 await read();
