ae9645782c3a2bf479413132daa6c7aa4b906f05
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-06 14:57:32 +0500

    feat: add read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import fs from "node:fs";
+import { getDirname } from "../fs/pathHelper.js";
+import { join } from "node:path";
+import { stdout } from "node:process";
+
+const __dirname = getDirname(import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+  const pathFileToRead = join(__dirname, "files", "fileToRead.txt");
+
+  const input = fs.createReadStream(pathFileToRead, { encoding: "utf-8" });
+  input.pipe(stdout);
 };
 
 await read();
