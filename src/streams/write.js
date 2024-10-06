c7acb41ce44c22c3cd9d1ec276b82ba32fa59d4f
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-06 15:11:03 +0500

    feat: add write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import fs from "node:fs";
+import { stdin } from "node:process";
+import { getDirname } from "../fs/pathHelper.js";
+import { join } from "node:path";
+
+const __dirname = getDirname(import.meta.url);
+
 const write = async () => {
-    // Write your code here 
+  const pathFileToRead = join(__dirname, "files", "fileToWrite.txt");
+
+  const output = fs.createWriteStream(pathFileToRead, { encoding: "utf-8" });
+
+  console.log(
+    "Please enter any text.\nTo terminate the program, press Ctrl + C:\n"
+  );
+
+  return new Promise((resolve, reject) => {
+    stdin.pipe(output);
+    output.on('finish', resolve);
+    output.on('error', reject);
+  })
 };
 
 await write();
