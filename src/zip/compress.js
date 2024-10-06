10f7c391460ff91bd5383e74e0b2460b8d4798f2
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-06 17:56:25 +0500

    feat: add compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+import { pipeline } from "node:stream";
+
+import { getDirname } from "../fs/pathHelper.js";
+import { join } from "node:path";
+
+const __dirname = getDirname(import.meta.url);
+
 const compress = async () => {
-    // Write your code here 
+  const pathFileToCompress = join(__dirname, "files", "fileToCompress.txt");
+  const pathFileToCompressed = join(__dirname, "files", "archive.gz");
+
+  const gzip = createGzip();
+  const source = createReadStream(pathFileToCompress);
+  const destnation = createWriteStream(pathFileToCompressed);
+  pipeline(source, gzip, destnation, (err) => {
+    if (err) {
+      throw new Error(err);
+    }
+    process.exitCode = 1;
+  });
 };
 
 await compress();
