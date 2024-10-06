f53e949d2bfbbf139e946a65010429094f9886a7
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-06 14:32:36 +0500

    feat: add calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createHash } from "node:crypto";
+import { getDirname } from "../fs/pathHelper.js";
+import { join } from "node:path";
+import fs from "node:fs";
+
+const __dirname = getDirname(import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const pathFileToCalc = join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+  const input = fs.createReadStream(pathFileToCalc);
+  input.on("readable", () => {
+    const data = input.read();
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log(hash.digest("hex"));
+    }
+  });
 };
 
 await calculateHash();
