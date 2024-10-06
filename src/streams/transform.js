1cdebee205659f10d250ea02a9ff8c1ab8d826c5
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-06 17:40:49 +0500

    feat: add transfrom

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from "node:stream";
+import { stdout, stdin } from "node:process";
+
+const reverse = new Transform({
+  transform(chunk, encoding, callback) {
+    const reveseChunk = chunk.toString().split("").reverse().join("");
+    callback(null, reveseChunk + "\n");
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  console.log(
+    "Please enter any text.\nTo terminate the program, press Ctrl + C:\n"
+  );
+  stdin.pipe(reverse).pipe(stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -16,7 +16,7 @@ const write = async () => {
 
   return new Promise((resolve, reject) => {
     stdin.pipe(output);
-    output.on('end', resolve);
+    output.on('finish', resolve);
     output.on('error', reject);
   })
 };
