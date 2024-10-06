497b65abc8a939a1d8699f14976908d62c10a1cc
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-06 19:46:06 +0500

    feat: add main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import { join } from "path";
+import { getDirname } from "../fs/pathHelper.js";
+
+const __dirname = getDirname(import.meta.url);
+
+import { Worker } from "node:worker_threads";
+import { cpus } from "node:os";
+
 const performCalculations = async () => {
-    // Write your code here
+  const promisesWorkers = [];
+  const workerPath = join(__dirname, "worker.js");
+  const cpuCount = cpus().length;
+
+  for (let i = 0; i <= cpuCount; i++) {
+    const data = 10 + i;
+
+    const promise = new Promise((resolve, reject) => {
+      const worker = new Worker(workerPath, { workerData: data === 11 ? 'wrong_data' : data });
+      worker.on("message", resolve);
+      worker.on("error", reject);
+      worker.on("exit", (code) => {
+        if (code !== 0)
+          reject(new Error(`Worker stopped with exit code ${code}`));
+      });
+    });
+    promisesWorkers.push(promise);
+  }
+
+  const result = await Promise.allSettled(promisesWorkers);
+  console.log(result);
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,4 +1,4 @@
-import {parentPort, workerData} from "node:worker_threads";
+import { parentPort, workerData } from "node:worker_threads";
 const nthFibonacci = (n) =>
   n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
