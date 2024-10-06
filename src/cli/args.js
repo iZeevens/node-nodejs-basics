5bb51774ece5a44c8c3d6c25aa84bce5d8dcc626
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-06 13:43:06 +0500

    feat: add args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const result = [];
+  const args = process.argv.slice(2);
+
+  for (let i = 0; i < args.length - 1; i += 2) {
+    result.push(`${args[i].slice(2)} is ${args[i + 1]}`);
+  }
+
+  console.log(result.join(', '));
 };
 
 parseArgs();
