d0f97731fe01c3911d8e3f64dab394e90dfd98fb
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-10-07 21:09:38 +0500

    refactor: create and copy to async

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,6 +1,6 @@
-import fs from "fs";
-import { join } from "path";
-import { getDirname } from './pathHelper.js';
+import fs from "node:fs/promises";
+import { join } from "node:path";
+import { getDirname } from "./pathHelper.js";
 
 const __dirname = getDirname(import.meta.url);
 
@@ -8,14 +8,15 @@ const create = async () => {
   const content = "I am fresh and young";
   const filePath = join(__dirname, "files", "fresh.txt");
 
-  if (fs.existsSync(filePath)) {
+  try {
+    await fs.access(filePath);
     throw new Error("FS operation failed");
-  } else {
-    fs.writeFile(filePath, content, (err) => {
-      if (err) {
-        throw new Error(err);
-      }
-    });
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fs.writeFile(filePath, content);
+    } else {
+      throw err;
+    }
   }
 };
 
